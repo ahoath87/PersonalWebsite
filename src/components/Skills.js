dc194a5c5284c1578fc3ff5fad9c61c80e0c0dd1
Ashley Hoath <ahoath87@gmail.com>
2023-02-14 13:50:15 -0700

    changed spelling error

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,41 +1,10 @@
 import React from 'react';
-import skills7 from '../assets/images/skills7.jpg';
+import skills6 from '../assets/images/skills6.jpg';
 
 const Skills = () => {
   return (
     <div id='skills'>
-      <img id='skillsimg' src={skills7}></img>
-      {/* <p id='skills-title'>Skills</p>
-      <div>
-        <p id='frontend'>Frontend Development</p>
-        <p>HTML</p>
-        <div className='skills-container'>
-          <div className='skills-html'></div>
-        </div>
-        <p>Javascript</p>
-        <div className='skills-container'>
-          <div className='skills-javascript'></div>
-        </div>
-        <p>React</p>
-        <div className='skills-container'>
-          <div className='skills-react'></div>
-        </div>
-      </div>
-      <div>
-        <p id='backend'>Backend development</p>
-        <p>MySQL</p>
-        <div className='skills-container'>
-          <div className='skills-sql'></div>
-        </div>
-        <p>Express</p>
-        <div className='skills-container'>
-          <div className='skills-express'></div>
-        </div>
-        <p>Node.js</p>
-        <div className='skills-container'>
-          <div className='skills-node'></div>
-        </div>
-      </div> */}
+      <img id='skillsimg' src={skills6}></img>
     </div>
   );
 };
